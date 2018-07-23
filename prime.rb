0fe169b67627300a660d192c7aad2cb85744468e
Adam Johnson <adamwjo@gmail.com>
2018-07-23 14:33:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,8 @@
 # Add  code here!
 def prime?(num)
-  array =* (-200..300000)
-  array.each do |i|
-    if num % i == 0
-      return false
-    else
-      return true
-    end
+  if num % i == 0
+    return false
+  else
+    return true
   end
 end
