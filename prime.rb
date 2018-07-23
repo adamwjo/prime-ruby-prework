52a3704b0a5664d43d9fe79cd60028c684323813
Adam Johnson <adamwjo@gmail.com>
2018-07-23 15:22:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,5 +6,15 @@ def prime?(num)
     return false
   elsif num > 0 && num < 4
     return true
-
+  elsif num % 2 == 0
+    return false
+  else
+    arr = (4..num/2).to_a
+    arr.each do |i|
+      if num % i == 0
+        return false
+      end
+    end
+  end
+    true
 end
